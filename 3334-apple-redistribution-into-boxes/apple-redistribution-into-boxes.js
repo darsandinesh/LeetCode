f828765e96dbcecd259dcf966eb9a0a0ece471d0
darsandinesh <98115926+darsandinesh@users.noreply.github.com>
2024-04-15 09:54:22 +0530

    Time: 62 ms (43.62%) | Memory: 50.7 MB (68.82%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.js
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.js
@@ -5,7 +5,10 @@
  */
 var minimumBoxes = function (apple, capacity) {
 
-    let totalApples = apple.reduce((acc, val) => acc + val, 0)
+    let totalApples = 0
+    for (let i = 0; i < apple.length; i++) {
+        totalApples += apple[i];
+    }
     capacity.sort((a, b) => b - a);
     for (let i = 0; i < capacity.length; i++) {
         totalApples = totalApples - capacity[i]
