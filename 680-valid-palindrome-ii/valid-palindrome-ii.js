7c83275f6d5796abfa27480b0299ce565ea89f6e
darsandinesh <98115926+darsandinesh@users.noreply.github.com>
2024-05-20 10:44:22 +0530

    Time: 82 ms (21.99%) | Memory: 55.1 MB (58.25%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function validPalindrome(s) {
+
+    const isPalindrome = (str, left, right) => {
+        while (left < right) {
+            if (str[left] !== str[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    };
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+           
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
