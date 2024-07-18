74696bfc1296dc9aa1273c2a56988c8d3f713e96
darsandinesh <98115926+darsandinesh@users.noreply.github.com>
2024-07-18 13:05:33 +0530

    Time: 53 ms (49.57%) | Memory: 48.9 MB (42.74%) - LeetSync

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} position
+ * @return {number}
+ */
+var minCostToMoveChips = function (position) {
+
+    let even = 0;
+    let odd = 0;
+    for (let i = 0; i < position.length; i++) {
+        if (position[i]%2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+    return Math.min(even, odd);
+
+};
