5877677be8ba2cf98d137756fb4d15fcc8904aa3
darsandinesh <98115926+darsandinesh@users.noreply.github.com>
2024-07-31 15:13:23 +0530

    Time: 43 ms (94.03%) | Memory: 48.9 MB (52.77%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,8 +3,8 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function (haystack, needle) {
+var strStr = function(haystack, needle) {
 
     return haystack.indexOf(needle)
-
+    
 };
