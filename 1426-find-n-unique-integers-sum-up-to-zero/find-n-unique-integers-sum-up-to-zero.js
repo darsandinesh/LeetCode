bb572f41b9dc2d6e05b2563f2347b7524101b407
darsandinesh <98115926+darsandinesh@users.noreply.github.com>
2024-08-23 09:41:28 +0530

    Time: 56 ms (62.75%) | Memory: 51.9 MB (15.10%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function (n) {
+
+    let arr = [];
+    if (n % 2 != 0) arr.push(0);
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        arr.push(-(i + 1));
+        arr.push((i + 1));
+    }
+    return arr.sort((a, b) => a - b)
+
+};
