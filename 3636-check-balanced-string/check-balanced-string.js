e8ced342de7e024bc1fe5bbaf7b02362741834fc
darsandinesh <98115926+darsandinesh@users.noreply.github.com>
2024-11-29 09:49:28 +0530

    Time: 1 ms (81.35%) | Memory: 51.3 MB (69.23%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isBalanced = function (num) {
+
+    let odd = 0;
+    let even = 0;
+    for (let i = 0; i < num.length; i++) {
+        if (i % 2 == 0) even += Number(num[i]);
+        else odd += Number(num[i]);
+    }
+    return odd === even
+};
